f1c6c8fa245f6d20a0f0f7e2ce978677815fdb31
Daizai <leo-im@hotmail.com>
2022-11-08 00:20:21 -0500

    nov 08 v1

--- pages/Home/Article.js
+++ pages/Home/Article.js
@@ -2,7 +2,7 @@
 //@ts-check
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 
-import { Autoplay, } from 'swiper'
+import { Autoplay, Pagination } from 'swiper'
 
 
 // Import Swiper styles
@@ -21,70 +21,66 @@ function Article(props) {
             <div className='d-flex mb-4 justify-content-between'>
                 <h2>Explora bicis destacadas</h2>
 
-                <div><button onClick={() => swiper.slideNext()}>Slide to the next slide</button></div>
+                <div></div>
             </div>
 
             <Swiper
-                modules={[Autoplay,]}
+                modules={[Autoplay, Pagination]}
 
                 autoplay={{
+                    delay: 5000
+                }}
+                pagination={{
+                    clickable: true,
                 }}
 
                 loop={true}
                 breakpoints={{
                     "@0.00": {
-                      slidesPerView: 1.15,
-                      spaceBetween: 25,
-                    },
-                    "@0.40": {
                         slidesPerView: 1.15,
                         spaceBetween: 25,
-                      },
-                    "@0.50": {
-                      slidesPerView: 1.25,
-                      spaceBetween: 25,
                     },
-                    "@0.60": {
+                    "@0.40": {
                         slidesPerView: 1.25,
                         spaceBetween: 25,
-                      },
+                    },
+                    "@0.50": {
+                        slidesPerView: 1.35,
+                        spaceBetween: 25,
+                    },
+                    "@0.60": {
+                        slidesPerView: 1.45,
+                        spaceBetween: 25,
+                    },
                     "@0.70": {
-                        slidesPerView: 1.30,
+                        slidesPerView: 1.50,
                         spaceBetween: 25,
-                      },
-                      "@0.80": {
-                        slidesPerView: 1.40,
-                        spaceBetween: 25,
-                      },
-                      "@0.90": {
+                    },
+                    "@0.80": {
                         slidesPerView: 1.75,
                         spaceBetween: 25,
-                      },
-                    "@1.00": {
-                      slidesPerView: 2.20,
-                      spaceBetween: 25,
+                    },
+                    "@0.90": {
+                        slidesPerView: 2.50,
+
                     },
                     "@1.10": {
-                        slidesPerView: 2.50,
+                        slidesPerView: 3,
                         spaceBetween: 25,
-                      },
-                    "@1.20": {
-                      slidesPerView: 3,
-                      spaceBetween: 20,
                     },
                     "@1.30": {
-                        slidesPerView: 3.25,
-                        spaceBetween: 20,
-                      },
-                    "@1.40": {
                         slidesPerView: 3.50,
-                        spaceBetween: 20,
-                      },
-                      "@1.50": {
+
+                    },
+                    "@1.40": {
                         slidesPerView: 4,
-                        spaceBetween: 20,
-                      },
-                  }}
+
+                    },
+                    "@1.50": {
+                        slidesPerView: 4,
+
+                    },
+                }}
 
                 slidesPerView="auto"
             >
