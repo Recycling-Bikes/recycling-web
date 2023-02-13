0f70427d31b2fbb0f9fc5c5d3ffe489b7e58c9d4
junowoz <junogouvea@gmail.com>
2023-02-13 19:36:28 -0400

    style

--- components/FormNovatos/img.js
+++ components/FormNovatos/img.js
@@ -3,15 +3,12 @@ import React from "react";
 import ImagePrincipal from "./unsplash_VfUN94cUy4o.png";
 
 export default function ImagePrueba() {
-  return (
-    <Image
-      src={ImagePrincipal}
-      alt=""
-      className="ratio ratio-1x1 d-none d-xl-block"
-      style={{ maxHeight: "800px",
-      maxWidth: "800px"
-
-     }}
-    />
-  );
+    return (
+        <Image
+            src={ImagePrincipal}
+            alt=""
+            className="ratio ratio-1x1 d-none d-xl-block"
+            style={{ maxHeight: "800px", maxWidth: "800px" }}
+        />
+    );
 }
--- components/FormNovatos/index.js
+++ components/FormNovatos/index.js
@@ -1,4 +1,6 @@
-import FormNovatosComponent, { Selects } from "components/FormNovatos/component";
+import FormNovatosComponent, {
+    Selects,
+} from "components/FormNovatos/component";
 import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
@@ -8,57 +10,67 @@ import { useRouter } from "next/router";
 import { MdKeyboardArrowLeft } from "react-icons/md";
 
 export default function FormNovatos({
-  questions,
-  title,
-  description,
-  onSubmit,
-  nameForm,
-  back,
-  progress
+    questions,
+    title,
+    description,
+    onSubmit,
+    nameForm,
+    back,
+    progress,
 }) {
-
     const router = useRouter();
-  const quest = formNovatosState((state) => state.quest);
+    const quest = formNovatosState((state) => state.quest);
 
-  const { handleSubmit, register, control } = useForm({
-    defaultValues: quest,
-  });
+    const { handleSubmit, register, control } = useForm({
+        defaultValues: quest,
+    });
 
-  const submit = useRef(null);
+    const submit = useRef(null);
 
-  const HandleClick = () => {
-    submit.current.click();
-  };
+    const HandleClick = () => {
+        submit.current.click();
+    };
 
-  return (
-    <FormNovatosComponent>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mt-5 d-none d-xl-block" />
+    return (
+        <FormNovatosComponent>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mt-5 d-none d-xl-block" />
 
-        {progress? <ProgressBar className="mt-5" now={progress} />: null}
-        <div className="mb-4"/>
-        <h1 className="mb-5" style={{ color: "#212928" }}>
-          {title}
-        </h1>
-        <h3 className="mb-3" style={{ color: "#212928" }}>{description}</h3>
+                {progress ? (
+                    <ProgressBar className="mt-5" now={progress} />
+                ) : null}
+                <div className="mb-4" />
+                <h1 className="mb-5" style={{ color: "#212928" }}>
+                    {title}
+                </h1>
+                <h3 className="mb-3" style={{ color: "#212928" }}>
+                    {description}
+                </h3>
 
-        {questions.map((data, index) => (
-          <Selects
-            register={register}
-            data={data}
-            index={index}
-            name={nameForm}
-            key={index}
-            HandleClick={HandleClick}
-            className="mb-3"
-          />
-        ))}
-        <Button variant="" onClick={()=> router.push(back)} style={{color: "#0FA899"}} > <MdKeyboardArrowLeft/> Atrás</Button>
+                {questions.map((data, index) => (
+                    <Selects
+                        register={register}
+                        data={data}
+                        index={index}
+                        name={nameForm}
+                        key={index}
+                        HandleClick={HandleClick}
+                        className="mb-3"
+                    />
+                ))}
+                <Button
+                    variant=""
+                    onClick={() => router.push(back)}
+                    style={{ color: "#0FA899" }}
+                >
+                    {" "}
+                    <MdKeyboardArrowLeft /> Atrás
+                </Button>
 
-        <div className="d-xl-none" style={{ height: "20vh" }} />
-        
-        <button ref={submit} type="submit" className="d-none" />
-      </form>
-    </FormNovatosComponent>
-  );
+                <div className="d-xl-none" style={{ height: "20vh" }} />
+
+                <button ref={submit} type="submit" className="d-none" />
+            </form>
+        </FormNovatosComponent>
+    );
 }
