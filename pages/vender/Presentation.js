86eb87093d6625ff86c31051be8c77140b7c02d9
junowoz <junogouvea@gmail.com>
2023-02-24 16:03:00 -0400

    presentation

--- pages/vender/Presentation.js
+++ pages/vender/Presentation.js
@@ -1,68 +1,62 @@
-
-import { Container, Row, Col, Button, Image, Card } from 'react-bootstrap';
-import Link from 'next/link';
-import { MdKeyboardArrowRight } from "react-icons/md"
-import { useRouter } from 'next/router';
-
-
-
+import { Container, Row, Col, Button, Image, Card } from "react-bootstrap";
+import Link from "next/link";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { useRouter } from "next/router";
 
 function Presentation() {
-    const router = useRouter()
+    const router = useRouter();
     return (
-
-
-        <Container className=''>
-            <Col   className=' m-2 pb-4  d-block d-lg-none m-1'>
-                    <div md={100} className=' text-center bg-image'>
-                        <Card.Img variant="top" src="/psintermedia.png" className='' 
-                        style={{borderRadius: "30px" }}
+        <>
+            <Container className="mt-3">
+                <Col className=" m-2 pb-4  d-block d-lg-none m-1">
+                    <div md={100} className=" text-center bg-image">
+                        <Card.Img
+                            variant="top"
+                            src="/psintermedia.png"
+                            className="rounded-5"
+                            // style={{ borderRadius: "30px" }}
                         />
-
-
                     </div>
                 </Col>
-            <Row className=''>
-
-            
-
-
-                <Col className='mx-2 d-flex flex-column justify-content-center align-items-baseline'>
-                    
-
-                            <h1 className='fs-1 tittle-custom'> 
-                            Vende tu bici de manera fácil y segura con nosotros</h1>
-                            <p className='pb-4'>
+                <Row className="">
+                    <Col className="mx-2 d-flex flex-column justify-content-center align-items-baseline">
+                        <h1 className="fs-1 tittle-custom">
+                            Vende tu bici de manera fácil y segura con nosotros
+                        </h1>
+                        <p className="pb-4 fw-semibold">
                             Publica en un mercado de más de 3.000 ciclistas
-                            </p>
-                            <Button className='' variant="primary" onClick={()=>{localStorage.removeItem('items'); router.push("/publicar/uno")}}>Vender tu bici ahora</Button>
-                                
-                                    
+                        </p>
+                        <Button
+                            className=""
+                            variant="primary"
+                            onClick={() => {
+                                localStorage.removeItem("items");
+                                router.push("/publicar/uno");
+                            }}
+                        >
+                            Vender bici ahora
+                        </Button>
+                    </Col>
 
-                                
+                    <Col className=" d-none d-lg-block m-1">
+                        <div className=" text-center bg-image">
+                            <Card.Img
+                                variant="top"
+                                src="/psintermedia.png"
+                                className="rounded-5"
+                                // style={{ borderRadius: "30px" }}
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
 
-                            
-
-
-                </Col>
-
-                <Col className=' d-none d-lg-block m-1'>
-                    <div
-                        className=' text-center bg-image'>
-                        <Card.Img variant="top" src="/psintermedia.png" className='' 
-                        style={{borderRadius: "30px 0 0 30px"}}
-                        />
-
-
-                    </div>
-                </Col>
-
-            </Row>
-
-        </Container>
-
-
-    )
-};
+            {/* Es Rapido y Sencillo */}
+            <Container>
+                
+            </Container>
+        </>
+    );
+}
 
 export default Presentation;
