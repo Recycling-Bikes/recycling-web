2cf1516fe84554e2c38a84383f929a61d1ee506c
junowoz <junogouvea@gmail.com>
2023-02-25 00:09:12 -0400

    new icons

--- pages/publicar/tres/index.js
+++ pages/publicar/tres/index.js
@@ -2,7 +2,7 @@ import React, {  useEffect } from "react";
 import Contenedor from "components/home/Contenedor";
 import { Row, Form, Col, Container, Button } from "react-bootstrap";
 import Link from "next/link";
-
+import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
 import { useRouter } from "next/router";
 
 import Progres3 from "./progres3";
@@ -55,56 +55,71 @@ export default function Parttres() {
   };
 
   return (
-    <Contenedor>
-      <DevTool control={control} />
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col md="8" xl="6">
-            <Form className=" py-5" onSubmit={handleSubmit(onSubmit)}>
-              <Progres3 />
-              <div className="mt-5">
-                <Container>
-                  {form?.conditions?.map((datum) => {
-                    return (
-                      <Form.Group key={datum.id} required>
-                        <div className="d-flex justify-content-sm-between flex-column flex-sm-row my-4 ">
-                          <Col className="mx=sm-4 my-2 my-sm-0">
-                            <Form.Check type="radio" id={`radio-${datum.id}`}>
-                              <Form.Check.Input
-                                required
-                                type="radio"
-                                value={datum.id}
-                                {...register("conditions")}
-                              />
+      <Contenedor>
+          <DevTool control={control} />
+          <Container>
+              <Row className="justify-content-md-center">
+                  <Col md="8" xl="6">
+                      <Form className=" py-5" onSubmit={handleSubmit(onSubmit)}>
+                          <Progres3 />
+                          <div className="mt-5">
+                              <Container>
+                                  {form?.conditions?.map((datum) => {
+                                      return (
+                                          <Form.Group key={datum.id} required>
+                                              <div className="d-flex justify-content-sm-between flex-column flex-sm-row my-4 ">
+                                                  <Col className="mx=sm-4 my-2 my-sm-0">
+                                                      <Form.Check
+                                                          type="radio"
+                                                          id={`radio-${datum.id}`}
+                                                      >
+                                                          <Form.Check.Input
+                                                              required
+                                                              type="radio"
+                                                              value={datum.id}
+                                                              {...register(
+                                                                  "conditions"
+                                                              )}
+                                                          />
 
-                              <Form.Check.Label>{datum.name}</Form.Check.Label>
-                            </Form.Check>
-                          </Col>
+                                                          <Form.Check.Label>
+                                                              {datum.name}
+                                                          </Form.Check.Label>
+                                                      </Form.Check>
+                                                  </Col>
 
-                          <Col sm="5" className="">
-                            {datum.description}
-                          </Col>
-                        </div>
-                      </Form.Group>
-                    );
-                  })}
-                </Container>
-              </div>
+                                                  <Col sm="5" className="">
+                                                      {datum.description}
+                                                  </Col>
+                                              </div>
+                                          </Form.Group>
+                                      );
+                                  })}
+                              </Container>
+                          </div>
 
-              <div className="d-flex justify-content-end pt-3 align-items-center">
-                <Link href="./two" className="mx-3" type="submit">
-                  Atras
-                </Link>
+                          <div className="d-flex justify-content-end pt-3 align-items-center">
+                              <Link href="./two" className="mx-3" type="submit">
+                                  <BsChevronLeft
+                                      size={18}
+                                      className="me-1 align-items-center"
+                                  />
+                                  Atrás
+                              </Link>
 
-                <Button variant="primary" type="submit">
-                  Valor de tu bici
-                </Button>
-              </div>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-      <div className="d-none d-lg-block" style={{ height: "10rem" }}/>
-    </Contenedor>
+                              <Button variant="primary" type="submit">
+                                  Valor de tu bici
+                                  <BsChevronRight
+                                      size={18}
+                                      className="ms-1 align-items-center"
+                                  />
+                              </Button>
+                          </div>
+                      </Form>
+                  </Col>
+              </Row>
+          </Container>
+          <div className="d-none d-lg-block" style={{ height: "10rem" }} />
+      </Contenedor>
   );
 }
