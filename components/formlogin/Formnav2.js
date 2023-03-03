970549dc646248e6a26cabc385ea7db92d839771
junowoz <junogouvea@gmail.com>
2023-03-03 11:07:28 -0400

    login ok

--- components/formlogin/Formnav2.js
+++ components/formlogin/Formnav2.js
@@ -5,11 +5,11 @@ import { Button, Container } from 'react-bootstrap'
 export default function Formnav() {
     return (
         <Link href="/form/register">
-        <Container fluid className='d-flex flex-row-reverse my-2 align-items-baseline '>
+        <Container fluid className='d-flex flex-row-reverse my-3 align-items-baseline '>
             <Button variant="primary" type="submit" className='mx-2'>
                 Regístrate
             </Button>
-            <p href="./register" style={{ color: "black" }}>¿Aún no tienes cuenta?</p>
+            <p href="./register" style={{ color: "black" }} className="d-sm-block d-none me-3">¿Aún no tienes cuenta?</p>
 
         </Container>
         </Link>
