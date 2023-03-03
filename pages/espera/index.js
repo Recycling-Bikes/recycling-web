c209faf19abe2d3576440109d6657a29dd860fba
junowoz <junogouvea@gmail.com>
2023-03-03 15:13:36 -0400

    espera changes

--- pages/espera/index.js
+++ pages/espera/index.js
@@ -1,13 +1,13 @@
-import AdvisorComponent, { Selects } from "components/Advisor/component";
+import EsperaComponent, { Selects } from "./component";
 import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
-import { advisorState } from "context/Advisor/AdvisorState";
-import { Button, ProgressBar } from "react-bootstrap";
+import { esperaState } from "context/Espera/EsperaState";
+import { Button, InputGroup, ProgressBar, Row } from "react-bootstrap";
 import { useRouter } from "next/router";
-import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import Form from "react-bootstrap/Form";
 
-export default function Advisor({
+export default function Espera({
     questions,
     title,
     description,
@@ -18,35 +18,51 @@ export default function Advisor({
     checkbox, // <--- Agrega la propiedad "checkbox"
     progress,
 }) {
-    const router = useRouter();
-    const quest = advisorState((state) => state.quest);
-
-    const { handleSubmit, register, control } = useForm({
-        defaultValues: quest,
-    });
-
-    const submit = useRef(null);
-
-    const HandleClick = () => {
-        submit.current.click();
-    };
 
     return (
-        <AdvisorComponent>
-
-            <form onSubmit={handleSubmit(onSubmit)} className="pt-2">
-                <div className="mt-5 d-none d-xl-block" />
-                {progress ? (
-                    <ProgressBar className="mt-5" now={progress} />
-                ) : null}
-                <div className="mb-4" />
-                <h1 className="mb-5" style={{ color: "#212928" }}>
-                    {title}
+        <>
+            <EsperaComponent>
+                <h1 style={{ color: "#06433D", fontStyle: "medium" }}>
+                    ¿Aún no encuentras la bici de tus sueños? ¡Te ayudamos a
+                    rodar!
                 </h1>
-                <h3 className="mb-3" style={{ color: "#212928" }}>
-                    {description}
-                </h3>
-                {questions.map((data, index) => (
+                <p className="mb-5 text-secondary">
+                    Cuéntanos más sobre lo que estás buscando, moveremos cielo y
+                    tierra por conseguirlo para ti :){" "}
+                </p>
+                <form>
+                    <Row className="d-flex flex-row">
+                        <InputGroup className="mb-3">
+                            <Button
+                                variant="primary"
+                                className=" "
+                                type="submit"
+                                onClick={() => {
+                                    router.push("/publicar/uno");
+                                }}
+                            >
+                                Comenzar
+                                <MdKeyboardArrowRight />
+                            </Button>
+                        </InputGroup>
+                    </Row>
+                </form>
+            </EsperaComponent>
+
+            {/* <EsperaComponent>
+                <form onSubmit={handleSubmit(onSubmit)} className="pt-2">
+                    <div className="mt-5 d-none d-xl-block" />
+                    {progress ? (
+                        <ProgressBar className="mt-5" now={progress} />
+                    ) : null}
+                    <div className="mb-4" />
+                    <h1 className="mb-5" style={{ color: "#212928" }}>
+                        {title}
+                    </h1>
+                    <h3 className="mb-3" style={{ color: "#212928" }}>
+                        {description}
+                    </h3>
+                     {questions.map((data, index) => (
                     <Selects
                         register={register}
                         data={data}
@@ -56,39 +72,32 @@ export default function Advisor({
                         HandleClick={HandleClick}
                         className="mb-3"
                     />
-                ))}
+                ))} 
+                     */}
 
-                {/* Button Back con condicionales de visibilidad*/}
-                {backButtonVision ? (
-                    <Button
-                        variant="light"
-                        onClick={() => back && router.push(back)}
-                        className="mb-5 pb-5 mb-xl-0 pb-xl-0"
-                        style={{
-                            color: "#0FA899",
-                            background: "none",
-                            border: "none",
-                            shadow: "none",
-                        }}
-                    >
-                        <MdKeyboardArrowLeft /> Atrás
-                    </Button>
-                ) : null}
-                {/* Button Back End*/}
+                    {/* Button Back con condicionales de visibilidad
+                    {backButtonVision ? (
+                        <Button
+                            variant="light"
+                            onClick={() => back && router.push(back)}
+                            className="mb-5 pb-5 mb-xl-0 pb-xl-0"
+                            style={{
+                                color: "#0FA899",
+                                background: "none",
+                                border: "none",
+                                shadow: "none",
+                            }}
+                        >
+                            <MdKeyboardArrowLeft /> Atrás
+                        </Button>
+                    ) : null}
+                    {
+                         Button Back End*/}
 
-                {/*Agrega el checkbox con condicionales de visibilidad */}
-                {checkbox ? (
-                    <Form.Check
-                        type="checkbox"
-                        id="checkbox"
-                        className="mb-5 pb-5 mb-xl-0 pb-xl-0"
-                        label="No volver a mostrar pregunta"
-                    />
-                ) : null}
-                {/* End checkbox */}
-
-                <button ref={submit} type="submit" className="d-none" />
-            </form>
-        </AdvisorComponent>
+                    {/* <button ref={submit} type="submit" className="d-none" />
+                </form> 
+            </EsperaComponent>
+        */}
+        </>
     );
 }
