324e9c3d0b8b26c99b7768ec8c6c7e96639fb1d8
junowoz <junogouvea@gmail.com>
2023-03-03 15:14:19 -0400

    imgaval

--- pages/avaluador/imgMin.js
+++ pages/avaluador/imgMin.js
@@ -4,16 +4,12 @@ import ImagePrincipal from "./min_VfUN94cUy4o.png";
 
 export default function ImageAvaluadorMin({style, ...props}) {
   return (
-    
-    <Image
-      src={ImagePrincipal}
-      alt=""
-      className="ratio ratio-16x9 p-0 img-fluid"
-      style={{ maxHeight: "800px",
-      maxWidth: "800px",
-      ...style
-     }}
-    />
+      <Image
+          src={ImagePrincipal}
+          alt="Min"
+          className="ratio ratio-16x9 p-0 img-fluid rounded-5 my-4 mx-4"
+          style={{ maxHeight: "800px", maxWidth: "800px", ...style }}
+      />
   );
 }
 
