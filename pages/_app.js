00afe770bf4741abda596a937a9aebadbc7a2f71
Daizaikun <leo-im@hotmail.com>
2023-06-30 10:13:10 -0500

    fix error meta pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/custom.scss";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import ButtonWhatsapp from "components/main/ButtonWhatsapp";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
 	const queryClient = new QueryClient();
