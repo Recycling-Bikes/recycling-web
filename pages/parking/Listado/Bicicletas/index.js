b9c3aee00b1359a3ca3f28e8e5e7f9d2ee8d1e04
dailaim <laiglesias.min@gmail.com>
2024-08-08 22:45:13 -0500

    chore: Update Bicicletas component with server-side pagination and UI improvements

--- pages/parking/Listado/Bicicletas/index.js
+++ pages/parking/Listado/Bicicletas/index.js
@@ -1,38 +1,42 @@
-import { Card, Button, Row, Col } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 import { BsBell } from "react-icons/bs";
-import GetBicis from "./bicis";
 import FiltersMobile from "../FiltersMobile";
+import GetBicis from "./bicis";
 
-export default function Bicicletas() {
-  return (
-    <div>
-      <Card className="my-3 lg w-100" style={{ width: "auto" }}>
-        <Card.Body>
-          <Row className=" align-items-center">
-            {" "}
-            <Col>
-              <Card.Title>Guarda tu búsqueda</Card.Title>
-              <Card.Text>
-                Filtra tus resultados, y recibe una notificación cuando lleguen
-                nuevos productos coincidentes.
-              </Card.Text>
-            </Col>
-            <Col lg="auto" className="d-grid gap-2 py-3">
-              <Button style={{ width: "auto" }} variant="outline-primary">
-                {/* //Add bell */}
-                <BsBell className="me-2" />
-                Notificarme
-              </Button>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+import { Suspense } from "react";
 
-      {/* FiltersMobile */}
-      <div className="justify-content-center">
-        <FiltersMobile className="d-block d-xl-none" />
-      </div>
-      <GetBicis />
-    </div>
-  );
+export default function Bicicletas({ page, initialPage }) {
+	return (
+		<div>
+			<Card className="my-3 lg w-100" style={{ width: "auto" }}>
+				<Card.Body>
+					<Row className=" align-items-center">
+						{" "}
+						<Col>
+							<Card.Title>Guarda tu búsqueda</Card.Title>
+							<Card.Text>
+								Filtra tus resultados, y recibe una notificación cuando lleguen
+								nuevos productos coincidentes.
+							</Card.Text>
+						</Col>
+						<Col lg="auto" className="d-grid gap-2 py-3">
+							<Button style={{ width: "auto" }} variant="outline-primary">
+								{/* //Add bell */}
+								<BsBell className="me-2" />
+								Notificarme
+							</Button>
+						</Col>
+					</Row>
+				</Card.Body>
+			</Card>
+
+			{/* FiltersMobile */}
+			<div className="justify-content-center">
+				<FiltersMobile className="d-block d-xl-none" />
+			</div>
+			<Suspense>
+				<GetBicis page={page} initialPage={initialPage} />
+			</Suspense>
+		</div>
+	);
 }
