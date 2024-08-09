1de4801e2c08dfaed241dfb3b202470715f7f8b0
dailaim <laiglesias.min@gmail.com>
2024-08-08 22:45:08 -0500

    feat: Update bicis.js for server-side pagination and improve UI

--- pages/parking/Listado/Bicicletas/bicis.js
+++ pages/parking/Listado/Bicicletas/bicis.js
@@ -1,67 +1,71 @@
 import { ComponenteBike } from "components/bicletas";
-import {useEffect, useState } from "react";
-import { Spinner, Pagination, Button} from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Pagination, Spinner } from "react-bootstrap";
 import Relleno from "utils/relleno";
 
 import { useBicisPaginations } from "hooks/get-bicis-paginations";
 import { useRouter } from "next/router";
 
 export default function GetBicis(props) {
-  const { data, isLoading, fetchNextPage  } = useBicisPaginations();
-  // estado para la pagina actual
-  const [currentPage, setCurrentPage] = useState(1);
-  const router = useRouter();
+	const [currentPage, setCurrentPage] = useState(props.page);
+	const { data, isLoading } = useBicisPaginations({
+		page: currentPage,
+		initialPage: props.initialPage,
+	});
+	// estado para la pagina actual
+	const router = useRouter();
+	// Obtener items por página
+	const nextPage = () => {
+		if (data?.next) {
+			router.push(`?page=${data.next}`);
+			setCurrentPage(data.next);
+		}
+	};
 
-  // Obtener items por página
-  const items = data?.pages?.[0]?.data ?? [];
+	const prevPage = () => {
+		if (data?.prev) {
+			router.push(`?page=${data.prev}`);
+			setCurrentPage(data.prev);
+		}
+	};
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-    router.push(`?page=${page}`);
-  };
+	return (
+		<>
+			<div
+				className="d-grid gap-3 my-4"
+				style={{
+					gridTemplateColumns: "repeat(auto-fit,minmax(14.8rem, 1fr))",
+				}}
+			>
+				{data?.data?.map((bici) => (
+					<ComponenteBike
+						key={bici.id}
+						id={bici.id}
+						name={bici.models?.name}
+						title={bici.title}
+						price={bici.price}
+						status={bici.status}
+						sold={bici.sold}
+						off={bici.off}
+						image={bici.filesUrl[0]}
+						etiqueta={bici.etiquetas?.name}
+						verified={bici.verified}
+					/>
+				))}
+				{isLoading && (
+					<Spinner
+						animation="border"
+						variant="secondary"
+						style={{
+							width: "200px",
+							height: "200px",
+							fontSize: "90px",
+						}}
+					/>
+				)}
+				<Relleno />
 
-  useEffect(() => {
-    fetchNextPage();
-  }, [router.query.page]);
-
-
-  return (
-    <>
-      <div
-        className="d-grid gap-3 my-4"
-        style={{
-          gridTemplateColumns: "repeat(auto-fit,minmax(14.8rem, 1fr))",
-        }}
-      >
-        {items.map((bici) => (
-          <ComponenteBike
-            key={bici.id}
-            id={bici.id}
-            name={bici.models?.name}
-            title={bici.title}
-            price={bici.price}
-            status={bici.status}
-            sold={bici.sold}
-            off={bici.off}
-            image={bici.filesUrl[0]}
-            etiqueta={bici.etiquetas?.name}
-            verified={bici.verified}
-          />
-        ))}
-        {isLoading && (
-          <Spinner
-            animation="border"
-            variant="secondary"
-            style={{
-              width: "200px",
-              height: "200px",
-              fontSize: "90px",
-            }}
-          />
-        )}
-        <Relleno />
-
-        {/* <Pagination>
+				{/* <Pagination>
         <Pagination.First onClick={() => handlePageChange(1)} />
         <Pagination.Prev onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} />
 				{[...Array(Math.ceil(items.length / itemsPerPage))].map((_, index) => (
@@ -76,25 +80,36 @@ export default function GetBicis(props) {
         <Pagination.Next onClick={() => handlePageChange(currentPage + 1)} />
         <Pagination.Last onClick={() => handlePageChange(Math.ceil(items.length / itemsPerPage))} />
       </Pagination> */}
-      </div>
-      <div className="d-flex justify-content-center gap-3">
-        <Button
-          variant="primary"
+			</div>
+			<div className="d-flex justify-content-center gap-3">
+				<Button
+					variant="primary"
 					size="lg"
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Pagina anterior
-        </Button>
-        <Button
-          variant="primary"
+					onClick={() => prevPage()}
+					disabled={
+						data?.prev === null ||
+						data?.prev === undefined ||
+						data?.prev <= 1 ||
+						currentPage <= 1
+					}
+				>
+					Pagina anterior
+				</Button>
+				<Button
+					variant="primary"
 					size="lg"
-          className="bg-blue"
-          onClick={() => handlePageChange(currentPage + 1)}
-        >
-          Pagina Siguiente
-        </Button>
-      </div>
-    </>
-  );
+					className="bg-blue"
+					disabled={
+						data?.next === null ||
+						data?.next === undefined ||
+						data?.next >= data?.paginationCount ||
+						currentPage >= (data?.paginationCount ?? 100)
+					}
+					onClick={() => nextPage()}
+				>
+					Pagina Siguiente
+				</Button>
+			</div>
+		</>
+	);
 }
