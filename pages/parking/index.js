511966e1efd0d9c2b613ab52e473e458873fb7fd
DanTech99 <antoniomcs21@gotmail.com>
2024-08-24 10:56:04 -0500

    chore: Remove wretch dependency and update Bicicletas component for server-side pagination and UI improvements

--- pages/parking/index.js
+++ pages/parking/index.js
@@ -3,26 +3,6 @@ import { Col, Container, Row } from "react-bootstrap";
 import Bicicletas from "./Listado/Bicicletas";
 import Filtro from "./Listado/Filtro";
 
-import wretch from "wretch";
-
-export const getServerSideProps = async (a, b) => {
-	const page = Number.isNaN(Number(a.query.page)) ? 1 : Number(a.query.page);
-
-	const isDev = process.env.NODE_ENV !== "production";
-
-	const protocol = isDev ? "http" : "https";
-
-	const initialPage = await wretch(
-		`${protocol}://${a.req.headers.host}/api/allbicis`,
-	)
-		.post({
-			currentPage: page,
-			pageSize: 24,
-		})
-		.json()
-		.catch((err) => undefined);
-	return { props: { page, initialPage } };
-};
 
 export default function Todasbicis(props) {
 	return (
@@ -36,7 +16,7 @@ export default function Todasbicis(props) {
 						<Filtro />
 					</Col>
 					<Col className="separador">
-						<Bicicletas page={props.page} initialPage={props.initialPage} />
+						<Bicicletas page={props.page}/>
 					</Col>
 				</Row>
 			</Container>
